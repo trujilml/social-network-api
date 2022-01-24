ece4a4cef088836fbcf2ae4c0d6cb9b570e83cb5
trujilml <mickeytrj@gmail.com>
2022-01-23 20:06:28 -0600

    initial commit - created initial folders, user file, and installed express and mongoose package to get started on the project

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+
+        email: {
+            type: String, 
+            required: true,
+            trim: true,
+            match:  [/.+@.+\..+/]
+            // validation: 
+        },
+        thoughts: {
+            type: String
+        },
+        friends: {
+            type: String
+        }
+    }
+);
+
+const User = model('User', UserSchema);
+
+module.exports = User;
