5d45827ebebc7a5963d2e14a25cbe9dd9c0622bd
trujilml <mickeytrj@gmail.com>
2022-01-24 20:55:02 -0600

    created the thought model - continue working on it and other files

---
+++ models/Thought.js
@@ -0,0 +1,62 @@
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: {
+            reactionSchema,
+        },
+        {
+            toJSON: {
+                getters: true
+            }
+        }
+    }
+);
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal)
+        },
+        {
+            toJSON: {
+                getters: true
+            }
+        }
+    }
+);
+
+
+
+module.exports = Thought;
