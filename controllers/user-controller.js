d340682f0775ad2272c2219649742fba2d67b561
trujilml <mickeytrj@gmail.com>
2022-01-28 21:31:07 -0600

    started user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,4 +1,21 @@
 const { User } = require('../models');
 
+const userController = {
+    getAllUsers(req, res) {
+        User.find({})
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .select('-__v')
+        .sort({_id: -1})
+        .then(userData => res.json(userData))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(500);
+        });
+    },
 
+    getUserById
+}
 module.exports = userController;
